6a0951b20ae33fa4d362d339e5fa95a1a1900fac
tpsantos2120 <thiagocroza472@gmail.com>
2020-11-09 15:54:38 +0000

    I added a section for all modals in all pages. Also, added frame in all modals in the footer to maintain consistency.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,28 +1,22 @@
+var formSerializedArray = {};
+var inputDetails = {};
+
 /* DISPLAY MODAL FOR GALLERY CARDS AND JOIN US LINK IN THE PAGE HEADER */
 $("#load-header").load("components/header/header-component.html");
 $("#load-footer").load("components/footer/footer-component.html");
-
-function loadModalComponent() {
-  $("#joinUsHeaderModal").load("components/modals/join-form-modal.html");
-  $("#joinUsFooterModal").load("components/modals/join-form-modal.html");
-  $("#productInformationModal").load(
-    "components/modals/product-information-modal.html"
-  );
-  $("#customerServiceModal").load(
-    "components/modals/customer-service-modal.html"
-  );
-  $("#privacyPolicyModal").load("components/modals/privacy-policy-modal.html");
-  $("#testimonialsModal").load("components/modals/testimonials-modal.html");
-}
-
-$(window).on("load", loadModalComponent);
+$("#joinUsModal").load("components/modals/join-form-modal.html");
+$("#productInformationModal").load(
+  "components/modals/product-information-modal.html"
+);
+$("#customerServiceModal").load(
+  "components/modals/customer-service-modal.html"
+);
+$("#privacyPolicyModal").load("components/modals/privacy-policy-modal.html");
+$("#testimonialsModal").load("components/modals/testimonials-modal.html");
 
 function displayModal() {
   const clickedLink = $(this).text();
-  alert(clickedLink);
-  if (clickedLink === "JOIN US") {
-    $(".joinUs").modal("show");
-  } else if (clickedLink === "Join Us Now") {
+  if (clickedLink === "JOIN US" || clickedLink === "Join Us Now") {
     $(".joinUs").modal("show");
   } else if (clickedLink === "Product Information") {
     $("#product-information").modal("show");
@@ -43,9 +37,6 @@ $(function () {
     $("#imgModal").modal("show");
   });
 });
-
-var formSerializedArray = {};
-var inputDetails = {};
 
 $(document).on("submit", "form", function (event) {
   var formID = event.target.id;
@@ -86,39 +77,9 @@ function deconstructFormArray(formSerializedArray) {
 }
 
 function displayJoinUsResponse(userDetails) {
-  $("#formResponseModal").load("components/modals/form-response-modal.html");
-  $(".joinUs").modal("hide");
-  setTimeout(() => {
-    $("#responseMessage").append(
-      "Thank you, for signing up with us! You will receive our newsletter to this email: ",
-      userDetails.joinEmail
-    );
-    $("#userResponse").modal("show");
-  }, 1000);
-}
-
-function displayBookingResponse(userDetails) {
-  $("#formResponseModal").load("components/modals/form-response-modal.html");
-  setTimeout(() => {
-    $("#responseMessage").append(
-      "Thank you ",
-      userDetails.firstName,
-      ", for booking with us! We can confirm your booking for the ",
-      userDetails.bookingDate,
-      " at ",
-      userDetails.bookingTime,
-      " for ",
-      userDetails.peopleQuantity,
-      " people."
-    );
-    $("#userResponse").modal("show");
-  }, 1000);
-}
-
-function displayContactResponse(userDetails) {
   $("#spinnerResponseModal").load("components/modals/spinner-modal.html");
   $("#formResponseModal").load("components/modals/form-response-modal.html");
-  //display spinner in 100ms, to give enough time for spinner modal and response modal to load.
+  $(".joinUs").modal("hide");
   setTimeout(() => {
     $("#spinnerResponse").modal({
       backdrop: "static",
@@ -126,11 +87,58 @@ function displayContactResponse(userDetails) {
       show: true,
     });
   }, 500);
-  //set spinner to hide 0.5s before response modal
+
   setTimeout(() => {
     $("#spinnerResponse").modal("hide");
   }, 5500);
-  //append dynamic text to response modal and display it
+  setTimeout(() => {
+    $("#responseTitle").append("Thank you!");
+    $("#responseMessage").append(
+      "Thank you, for getting in contact with us! You will receive a response to your message as soon as possible to: ",
+      userDetails.joinEmail
+    );
+    $("#userResponse").modal("show");
+  }, 6000);
+}
+
+function displayBookingResponse(userDetails) {
+  $("#spinnerResponseModal").load("components/modals/spinner-modal.html");
+  $("#formResponseModal").load("components/modals/form-response-modal.html");
+  setTimeout(() => {
+    $("#spinnerResponse").modal({
+      backdrop: "static",
+      keyboard: false,
+      show: true,
+    });
+  }, 500);
+  setTimeout(() => {
+    $("#spinnerResponse").modal("hide");
+  }, 5500);
+  setTimeout(() => {
+    $("#responseTitle").append("Thank you!");
+    $("#responseMessage").append(
+      "Thank you ",
+      userDetails.fullName,
+      ", for getting in contact with us! You will receive a response to your message as soon as possible to: ",
+      userDetails.contactEmail
+    );
+    $("#userResponse").modal("show");
+  }, 6000);
+}
+
+function displayContactResponse(userDetails) {
+  $("#spinnerResponseModal").load("components/modals/spinner-modal.html");
+  $("#formResponseModal").load("components/modals/form-response-modal.html");
+  setTimeout(() => {
+    $("#spinnerResponse").modal({
+      backdrop: "static",
+      keyboard: false,
+      show: true,
+    });
+  }, 500);
+  setTimeout(() => {
+    $("#spinnerResponse").modal("hide");
+  }, 5500);
   setTimeout(() => {
     $("#responseTitle").append("Thank you!");
     $("#responseMessage").append(
